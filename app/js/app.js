b5734e5a65447090645d0076853dfa958b8ead78
Michael Gilliland <mjg.py3@gmail.com>
2014-12-13 00:32:01 -0500

    T

--- app/js/app.js
+++ app/js/app.js
@@ -3,15 +3,17 @@ var app = angular.module('codeBuddy', ['controllers']);
 var controllers = angular.module('controllers', []);
 
 controllers.controller('PersonController', ['$scope', '$http', function ($scope, $http) {
-    $scope.name = "mjgpy3";
+    $scope.name = '';
     $scope.events = [];
 
-    $http.get('https://api.github.com/users/' + $scope.name + '/events').
-      success(function(data, status, headers, config) {
-          $scope.events = data;
-      }).
-      error(function(data, status, headers, config) {
-          $scope.error = "Could not get activity for '" + $scope.name + "'";
-          console.log($scope.error);
-      });
+    $scope.findEvents = function () {
+        $http.get('https://api.github.com/users/' + $scope.name + '/events').
+          success(function(data, status, headers, config) {
+              $scope.events = data;
+          }).
+          error(function(data, status, headers, config) {
+              $scope.error = "Could not get activity for '" + $scope.name + "'";
+              console.log($scope.error);
+          });
+    }
 }]);
