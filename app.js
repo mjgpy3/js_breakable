1dc48c6927b45ab847204bc8dea846b36d9a6bbc
Michael Gilliland <mjg.py3@gmail.com>
2014-12-13 00:01:03 -0500

    Fixed missing curly

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function () {
-      console.log('Express server listening on port ' + app.get('port'));
-);
+    console.log('Express server listening on port ' + app.get('port'));
+});
