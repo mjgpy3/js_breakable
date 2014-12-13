936799b7a0fd92d2e7c0e567d69f36d8e3540c29
Michael Gilliland <mjg.py3@gmail.com>
2014-12-13 16:18:59 -0500

    Added breaking code just for testing

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
-  // yay!
+    this.should.break;
 });
 
 app.set('port', (process.env.PORT || 5000));
