88f16867784e1943b19b5ba9a377ca6d315daa95
Michael Gilliland <mjg.py3@gmail.com>
2014-12-13 00:19:54 -0500

    Tried adding http call

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,14 @@
-var PersonController = function ($scope) {
+var PersonController = function ($scope, $http) {
     $scope.name = "Some Person";
+
+    $http.get('https://api.github.com/users/' + $scope.name + '/events').
+      success(function(data, status, headers, config) {
+          $scope.events = data;
+      }).
+      error(function(data, status, headers, config) {
+          $scope.error = "Could not get activity for '" + $scope.name + "'";
+      });
 };
 
 angular.module('codeBuddy', []).
-    controller('PersonController', ['$scope', PersonController]);
+  controller('PersonController', ['$scope', PersonController]);
