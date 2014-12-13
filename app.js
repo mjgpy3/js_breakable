4e6f4c7afe566a33169c065950f3ee30b9388820
Michael Gilliland <mjg.py3@gmail.com>
2014-12-12 23:37:44 -0500

    Tried moving and re-assigning to app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+var PersonController = function ($scope) {
+    $scope.username = "Some Person";
+    $scope.events = [];
+};
+
+angular.module('codeBuddy', ['restangular']).
+    controller('PersonController', ['$scope', PersonController]);
